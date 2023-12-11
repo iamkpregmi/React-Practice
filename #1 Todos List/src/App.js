fe04ab262c0daa3b14869478570e3a6df23553fe
Krishna Prasad Regmi <kpkrishna309@gmail.com>
2023-12-11 19:10:28 +0530

    Add files via upload

---
+++ #1 Todos List/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Welcome to the first React App</p>
+      <button type="button" class="btn btn-primary btn-lg">Print</button>
+    </>
+  );
+}
+
+export default App;
